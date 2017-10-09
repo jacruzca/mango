9978dc609ed1fc86aa40386e35fae06cb95f80fc
jhon.cruz <jhon@beepquest.com>
2017-10-08 21:23:01 -0500

    Finished registration form

---
+++ lib/mango/crm/crm.ex
@@ -0,0 +1,16 @@
+defmodule Mango.CRM do
+  alias Mango.CRM.Customer
+  alias Mango.Repo
+
+  def build_customer(attrs \\ %{}) do
+    %Customer{}
+    |> Customer.changeset(attrs)
+  end
+
+  def create_customer(attrs) do
+    attrs
+    |> build_customer
+    |> Repo.insert
+  end
+
+end
